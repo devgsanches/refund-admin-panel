303636890ce88a079acd97a5c9d8d71e0fe49565
Guilherme Sanches <guilhermesanches.pessoal@gmail.com>
2025-07-11 16:50:03 -0300

    fix: routes

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -2,15 +2,19 @@ import { AuthLayout } from '@/layouts/Auth'
 import { SignIn } from '@/pages/auth/login'
 import { SignUp } from '@/pages/auth/register'
 import { NotFound } from '@/pages/notFound'
-import { Routes, Route } from 'react-router'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 export function AuthRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/auth/login" replace />} />
+
       <Route path="/auth" element={<AuthLayout />}>
+        <Route index element={<Navigate to="login" replace />} />
         <Route path="login" element={<SignIn />} />
         <Route path="register" element={<SignUp />} />
       </Route>
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
